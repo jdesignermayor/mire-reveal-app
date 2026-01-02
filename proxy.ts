4f7f674eb6a4f6b0df96f64801983fc50654bafc
joseluis <jdesignermayor@gmail.com>
2026-01-02 13:07:35 -0500

    feat: basic components added

---
+++ proxy.ts
@@ -0,0 +1,9 @@
+import type { NextRequest } from 'next/server';
+import { updateSession } from './lib/supabase/middleware';
+
+export async function proxy(request: NextRequest) {
+  return await updateSession(request);
+}
+export const config = {
+  matcher: ['/dashboard/:path*', '/checkout/:path*', '/profile/:path*', '/login'],
+};
