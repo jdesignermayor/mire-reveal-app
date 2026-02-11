bf859cb0b491714e353043ca5ef1159c244a0e51
joseluis <jdesignermayor@gmail.com>
2026-02-11 17:59:48 -0500

    feat: button color change

--- components/shared/LandingBody.tsx
+++ components/shared/LandingBody.tsx
@@ -26,7 +26,7 @@ export const LandingBody = () => {
                             trigger={
                                 <button
                                     type="button"
-                                    className="relative group inline-flex bg-gradient-to-r from-[#7e96ff] to-[#6b8cff] text-white px-6 py-3 rounded-2xl text-lg 2xl:text-2xl font-medium overflow-hidden transition-all duration-300 hover:shadow-lg hover:shadow-[#3e62ff]/25 hover:scale-105 active:scale-95"
+                                    className="relative rounded-2xl w-full md:w-auto group bg-gradient-to-r from-[#3e62ff] to-[#6b8cff] text-white px-6 py-3 text-xl font-medium overflow-hidden transition-all duration-300 hover:shadow-lg hover:shadow-[#3e62ff]/25 hover:scale-105 active:scale-95"
                                 >
                                     Book a demo
                                 </button>
