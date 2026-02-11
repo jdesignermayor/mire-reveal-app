ef2119f85934f96049573f11c1165ee328176398
joseluis <jdesignermayor@gmail.com>
2026-02-11 17:05:01 -0500

    feat: ui fixes

--- app/dashboard/create-illustration/page.tsx
+++ app/dashboard/create-illustration/page.tsx
@@ -1,8 +1,8 @@
 import RevealIllustrationForm from "@/components/features/illustration/RevealIllustrationForm";
 
 export default function IllustrationPage() {
-    return <section className="flex flex-col gap-8 w-full p-5 dark">
-          <div className="grid gap-8 dark">
+    return <section className="flex w-full dark justify-center h-dvh">
+          <div className="flex flex-col gap-2 dark max-w-7xl mx-auto py-5">
             <div>
               <h3 className="text-xl font-bold">Reveal ultrasound</h3>
               <p className="text-muted-foreground leading-relaxed">
